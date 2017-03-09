d444bc21cbee023b78d3416f4421bf99b269e070
Andy James <andrewhayesjames@gmail.com>
2017-03-08 19:48:44 -0500

    Broken save

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+	root "tweets#index"
+
+  get "feed" => "profiles#feed"
+
+  get 'likes/create'
+
+get "profiles/:id" => "profiles#show", as: :profile
+
+get "profiles" => "profiles#index"
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tweets
+resources :tweets do 
+  resource :like
+end
+
+resources :relationships
+
 end
